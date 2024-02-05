f15660047cc2fce00bc2f4be4592aa6463bb581c
Harley <harley.jones1992@gmail.com>
2024-02-05 08:47:31 -1000

    Working on Twitch integraion, currently broken

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,20 @@
 use log::{debug, error, info, trace, warn};
 use std::time::SystemTime;
 use humantime::Rfc3339Timestamp;
+use rand::Rng;
 
+
+pub fn random_number(max: i32) -> i32 {
+    // We intended max to be the .len() of the vector/array of users eligible.
+    let mut result = rand::thread_rng().gen_range(0..max);
+    result
+}
+
+pub fn select_winner(user_list: Vec<String>) -> String {
+    let winner = &user_list[random_number(user_list.len().try_into().unwrap()) as usize];
+    info!("Winner was: {} - {:?}", &winner, user_list);
+    winner.to_string()
+}
 
 pub fn setup_logger() -> Result<(), fern::InitError> {
     fern::Dispatch::new()
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,42 @@
 use log::{info};
+use tmi::{Channel, Client, Message};
+use anyhow::Result;
 
 fn main() {
     faucet::setup_logger();
-
+    let channels = Channel ("nimiqlive");
+    run(channels);
     let mut eligible_users: Vec<String> = vec![];
-
     // Just some logic to generate random sized arrays. 
-    let mut eligible_amount = random_number(100);
+    let mut eligible_amount = faucet::random_number(100);
     while eligible_amount > 0 {
         eligible_users.push("User_".to_string() + &eligible_amount.to_string());
         eligible_amount -= 1;
     }
     //
-
-    println!("Eligible users: {:?}", eligible_users);
     if eligible_users.len() == 0 { println!("No eligible users."); info!("No eligible users."); }
-    else { println!("Winner: {}", select_winner(eligible_users)); }
+    else { println!("Winner: {}", faucet::select_winner(eligible_users)); }
 
 }
 
-
-
-use rand::Rng;
-
-
-fn select_winner(user_list: Vec<String>) -> String {
-    let winner = &user_list[random_number(user_list.len().try_into().unwrap()) as usize];
-    info!("Winner was: {} - {:?}", &winner, user_list);
-    winner.to_string()
-}
-
-fn random_number(max: i32) -> i32 {
-    // We intended max to be the .len() of the vector/array of users eligible.
-    let mut result = rand::thread_rng().gen_range(0..max);
-    result
-}
+async fn run(channels: &[tmi::Channel]) -> anyhow::Result<()> {
+    let mut client = tmi::Client::connect().await?;
+    client.join_all(channels).await?;
+  
+    loop {
+      let msg = client.recv().await?;
+      match msg.as_typed()? {
+        tmi::Message::Privmsg(msg) => {
+          println!("{}: {}", msg.sender().name(), msg.text());
+        }
+        tmi::Message::Reconnect => {
+          client.reconnect().await?;
+          client.join_all(channels).await?;
+        }
+        tmi::Message::Ping(ping) => {
+          client.pong(&ping).await?;
+        }
+        _ => {}
+      };
+    }
+  }

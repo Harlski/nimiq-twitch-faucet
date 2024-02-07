f31ed63f25ed762a55a873a339b4c4021ba57bd5
Harley <harley.jones1992@gmail.com>
2024-02-07 17:31:44 +1100

    Reading Twitch Chat Works

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ use std::time::SystemTime;
 use humantime::Rfc3339Timestamp;
 use rand::Rng;
 
-
 pub fn random_number(max: i32) -> i32 {
     // We intended max to be the .len() of the vector/array of users eligible.
     let mut result = rand::thread_rng().gen_range(0..max);
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,60 @@
 use log::{info};
 use tmi::{Channel, Client, Message};
 use anyhow::Result;
+use tokio::main;
+// fn main() {
+//     faucet::setup_logger();
 
-fn main() {
+//     let mut eligible_users: Vec<String> = vec![];
+//     // Just some logic to generate random sized arrays. 
+//     let mut eligible_amount = faucet::random_number(100);
+//     while eligible_amount > 0 {
+//         eligible_users.push("User_".to_string() + &eligible_amount.to_string());
+//         eligible_amount -= 1;
+//     }
+//     //
+//     if eligible_users.len() == 0 { println!("No eligible users."); info!("No eligible users."); }
+//     else { println!("Winner: {}", faucet::select_winner(eligible_users)); }
+
+// }
+
+#[tokio::main]
+async fn main() -> Result<()> {
     faucet::setup_logger();
-    let channels = Channel ("nimiqlive");
-    run(channels);
-    let mut eligible_users: Vec<String> = vec![];
-    // Just some logic to generate random sized arrays. 
-    let mut eligible_amount = faucet::random_number(100);
-    while eligible_amount > 0 {
-        eligible_users.push("User_".to_string() + &eligible_amount.to_string());
-        eligible_amount -= 1;
-    }
-    //
-    if eligible_users.len() == 0 { println!("No eligible users."); info!("No eligible users."); }
-    else { println!("Winner: {}", faucet::select_winner(eligible_users)); }
 
+    let channels = vec![
+        Channel::parse("#nimiqlive".to_string()),
+        // Add more channels as needed
+    ];
+
+    let channels: Vec<tmi::Channel> = channels
+    .into_iter()
+    .filter_map(Result::ok)
+    .collect();
+  
+    run(&channels).await?;
+    Ok(())
 }
 
+
 async fn run(channels: &[tmi::Channel]) -> anyhow::Result<()> {
-    let mut client = tmi::Client::connect().await?;
-    client.join_all(channels).await?;
-  
-    loop {
-      let msg = client.recv().await?;
-      match msg.as_typed()? {
-        tmi::Message::Privmsg(msg) => {
-          println!("{}: {}", msg.sender().name(), msg.text());
-        }
-        tmi::Message::Reconnect => {
-          client.reconnect().await?;
-          client.join_all(channels).await?;
-        }
-        tmi::Message::Ping(ping) => {
-          client.pong(&ping).await?;
-        }
-        _ => {}
-      };
-    }
+  let mut client = tmi::Client::connect().await?;
+  client.join_all(channels).await?;
+
+  loop {
+    let msg = client.recv().await?;
+    match msg.as_typed()? {
+      tmi::Message::Privmsg(msg) => {
+        info!("{}: {}", msg.sender().name(), msg.text());
+      }
+      tmi::Message::Reconnect => {
+        client.reconnect().await?;
+        client.join_all(channels).await?;
+      }
+      tmi::Message::Ping(ping) => {
+        client.pong(&ping).await?;
+      }
+      _ => {}
+    };
   }
+}

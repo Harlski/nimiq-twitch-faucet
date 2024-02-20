db4c7ebffe46cb327c4c8a8169088fcc7d927db8
Harley <harley.jones1992@gmail.com>
2024-02-20 23:43:35 +1100

    Not much today sadly

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -4,7 +4,7 @@ use faucet::NlUser;
 pub fn do_something(user: NlUser) -> redis::RedisResult<()> {
     let client = redis::Client::open("")?;
     let mut con = client.get_connection()?;
-
+ 
     
     let _ : () = redis::cmd("SET").arg("my_key").arg(42).query(&mut con)?;
 

e4b1679c9ecd964354f3782e64fc42b54431f23c
Harley <harley.jones1992@gmail.com>
2024-02-23 19:24:42 -1000

    No work on this, but other non-git side project

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -5,7 +5,7 @@
 //     let client = redis::Client::open("")?;
 //     let mut con = client.get_connection()?;
    
-    
+     
 //     let _ : () = redis::cmd("SET").arg("my_key").arg(42).query(&mut con)?;
 
 //     Ok(())
